1314f5da7560e706e3ee8440ca260400b03e80dd
alpha14 <alpha14@users.noreply.github.com>
2023-03-27 16:27:09 +0200

    fix: user path for pip and rework pkg install

--- requirements.sh
+++ requirements.sh
@@ -5,6 +5,11 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
+CURRENT_USER="root"
+if [[ "${SUDO_USER}" != "" ]]; then
+    CURRENT_USER="${SUDO_USER}"
+fi
+
 # Install deps for debian based distros
 apt install laptop-detect software-properties-common -y
 
@@ -33,7 +38,7 @@ fi
 
 # Ubuntu specific
 if [[ "${_DISTRO}" == "ubuntu" ]]; then
-    add-apt-repository multiverse
+    add-apt-repository multiverse -y
     add-apt-repository ppa:cappelikan/ppa -y
 fi
 
@@ -49,34 +54,36 @@ else
     apt update
 fi
 
-apt install snapd terminator zsh keychain htop emacs golang whois dnsutils snapd traceroute build-essential imagemagick bpython git curl acpi-call-dkms tree xclip -y
+apt install python3-pip snapd terminator zsh keychain htop emacs whois dnsutils snapd traceroute build-essential imagemagick bpython git curl acpi-call-dkms tree xclip -y
 
-if [[ "${_IS_WINDOWS}" = false ]]; then
-    apt install nextcloud-desktop keepassxc python3-pip iftop iotop powertop ffmpeg -y
-    snap install firefox signal-desktop spotify onlyoffice-desktopeditors mattermost-desktop
-    snap install go --classic
+# Pyenv deps
+apt install build-essential libssl-dev zlib1g-dev \
+    libbz2-dev libreadline-dev libsqlite3-dev curl \
+    libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev \
+    libffi-dev liblzma-dev libpq-dev -y
 
-    # Pyenv deps
-    apt install build-essential libssl-dev zlib1g-dev \
-         libbz2-dev libreadline-dev libsqlite3-dev curl \
-         libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev \
-         libffi-dev liblzma-dev libpq-dev -y
 
-    pip install --user poetry yt-dlp
+runuser "${CURRENT_USER}" pip install --user poetry yt-dlp
+snap install go --classic
 
-    if [[ ("${_DISTRO}" == "ubuntu") ]]; then
-        apt install ubuntu-restricted-extras mainline -y
-    fi
+# No snap auto-updates, we want to control when updates are installed
+snap refresh --hold=forever
 
-    # Gnome settings
-    gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
-    gsettings set org.gnome.desktop.interface show-battery-percentage true
-    gsettings set org.gnome.desktop.interface enable-animations false
-
-    # No snap auto-updates, we want to control when updates are installed
-    snap refresh --hold=forever
+if [[ "${_IS_WINDOWS}" == true ]]; then
+    exit 0
 fi
 
+apt install nextcloud-desktop keepassxc iftop iotop powertop ffmpeg -y
+snap install firefox signal-desktop spotify onlyoffice-desktopeditors mattermost-desktop
+
+if [[ ("${_DISTRO}" == "ubuntu") ]]; then
+    apt install ubuntu-restricted-extras mainline -y
+fi
+
+# Gnome settings
+gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
+gsettings set org.gnome.desktop.interface show-battery-percentage true
+gsettings set org.gnome.desktop.interface enable-animations false
 
 # https://fwupd.org/lvfs/devices/
 fwupdmgr refresh
