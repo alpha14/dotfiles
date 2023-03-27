b2daa75772f192895b66a01881748a3177499738
alpha14 <alpha14@users.noreply.github.com>
2023-03-27 12:33:36 +0200

    feat: add more packages and remove power-profiles-daemon

--- requirements.sh
+++ requirements.sh
@@ -27,13 +27,14 @@ echo "Adding extra repositories"
 
 # Debian specific
 if [[ "${_DISTRO}" == "debian" ]]; then
-    apt-add-repository non-free
-    apt-add-repository contriub
+    add-apt-repository non-free
+    add-apt-repository contriub
 fi
 
 # Ubuntu specific
 if [[ "${_DISTRO}" == "ubuntu" ]]; then
-    apt-add-repository multiverse
+    add-apt-repository multiverse
+    add-apt-repository ppa:cappelikan/ppa -y
 fi
 
 echo "Installing packages"
@@ -41,16 +42,17 @@ echo "Installing packages"
 # Ubuntu & laptop specific
 if [[ ("${_IS_LAPTOP}" = true && "${_DISTRO}" == "ubuntu") ]]; then
     echo "laptop & distro"
+    apt remove power-profiles-daemon -y
     add-apt-repository ppa:slimbook/slimbook -y
     apt update && apt install slimbookbattery -y
 else
     apt update
 fi
 
-apt install snapd terminator zsh keychain htop emacs golang whois dnsutils snapd traceroute build-essential imagemagick bpython git curl -y
+apt install snapd terminator zsh keychain htop emacs golang whois dnsutils snapd traceroute build-essential imagemagick bpython git curl acpi-call-dkms tree xclip -y
 
 if [[ "${_IS_WINDOWS}" = false ]]; then
-    apt install nextcloud-desktop keepassxc python3-pip iftop iotop powertop -y
+    apt install nextcloud-desktop keepassxc python3-pip iftop iotop powertop ffmpeg -y
     snap install firefox signal-desktop spotify onlyoffice-desktopeditors mattermost-desktop
     snap install go --classic
 
@@ -58,12 +60,12 @@ if [[ "${_IS_WINDOWS}" = false ]]; then
     apt install build-essential libssl-dev zlib1g-dev \
          libbz2-dev libreadline-dev libsqlite3-dev curl \
          libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev \
-         libffi-dev liblzma-dev libpq-dev ffmpeg -y
+         libffi-dev liblzma-dev libpq-dev -y
 
     pip install --user poetry yt-dlp
 
     if [[ ("${_DISTRO}" == "ubuntu") ]]; then
-        apt install ubuntu-restricted-extras -y
+        apt install ubuntu-restricted-extras mainline -y
     fi
 
     # Gnome settings
@@ -73,11 +75,17 @@ if [[ "${_IS_WINDOWS}" = false ]]; then
     snap refresh --hold=forever
 fi
 
-update-initramfs -u -v
 
 # https://fwupd.org/lvfs/devices/
 fwupdmgr refresh
 fwupdmgr update
+
+# To trigger if:
+# - An updated kernel is installed
+# - If you have third-party kernel modules added/removed (drivers)
+# - The scripts responsible for creating the actual initramfs contents, or the configuration files for those scripts, are changed
+# - Any persistent settings related to the root filesystem, primary swap and/or resuming from hibernation are changed
+update-initramfs -u
 
 echo "Set term to use ZFS by default, run in your terminal:"
 echo "chsh -s /bin/zsh"
