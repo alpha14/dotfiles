551097cbd414879e35b1e3fec93e604979fcdda1
alpha14 <alpha14@users.noreply.github.com>
2025-01-27 17:05:38 +0100

    feat: use gnome tweaks if possible & refactor

--- requirements.sh
+++ requirements.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# This script is for debian based linux distros
+
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root"
    exit 1
@@ -25,36 +27,43 @@ if grep -q Microsoft /proc/version; then
     echo "Detected Windows operationg system"
 fi
 
+_IS_GNOME=false
+if which gnome-shell &>/dev/null; then
+    _IS_GNOME=true
+    echo "Detected GNOME"
+fi
+
 _DISTRO=$(awk -F= '/^ID=/{print $2}' /etc/os-release)
 echo "Detected linux distro : $_DISTRO"
 
-echo "Adding extra repositories"
 
 # Debian specific
 if [[ "${_DISTRO}" == "debian" ]]; then
+    echo "Adding extra deb repositories"
     add-apt-repository non-free
     add-apt-repository contriub
 fi
 
 # Ubuntu specific
 if [[ "${_DISTRO}" == "ubuntu" ]]; then
+    echo "Adding extra ubuntu repositories"
     add-apt-repository multiverse -y
+    # Mainline kernels
     add-apt-repository ppa:cappelikan/ppa -y
 fi
 
 echo "Installing packages"
 
 # Ubuntu & laptop specific
-if [[ ("${_IS_LAPTOP}" = true && "${_DISTRO}" == "ubuntu") ]]; then
+if [[ ("${_IS_LAPTOP}" == true && "${_DISTRO}" == "ubuntu") ]]; then
     echo "laptop & distro"
-    apt remove power-profiles-daemon -y
-    add-apt-repository ppa:slimbook/slimbook -y
-    apt update && apt install slimbookbattery -y
+    apt update
+    
 else
     apt update
 fi
 
-apt install python3-pip snapd tlp terminator zsh keychain htop emacs whois dnsutils snapd traceroute build-essential imagemagick bpython git curl acpi-call-dkms tree moreutils xclip -y
+apt install python3-pip zsh keychain htop emacs whois dnsutils traceroute build-essential imagemagick bpython git curl acpi-call-dkms tree moreutils xclip -y
 
 # Pyenv deps
 apt install build-essential libssl-dev zlib1g-dev \
@@ -62,28 +71,33 @@ apt install build-essential libssl-dev zlib1g-dev \
     libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev \
     libffi-dev liblzma-dev libpq-dev -y
 
-
 runuser "${CURRENT_USER}" pip install --user poetry yt-dlp
-snap install go --classic
-
-# No snap auto-updates, we want to control when updates are installed
-snap refresh --hold=forever
 
 if [[ "${_IS_WINDOWS}" == true ]]; then
+    echo "Minimal deps installed for Windows, exiting"
     exit 0
 fi
 
-apt install nextcloud-desktop keepassxc iftop iotop powertop ffmpeg -y
-snap install firefox signal-desktop spotify onlyoffice-desktopeditors mattermost-desktop
+
+apt install snapd acpi-call-dkms terminator nextcloud-desktop keepassxc iftop iotop powertop ffmpeg nethogs -y
+snap install firefox spotify onlyoffice-desktopeditors mattermost-desktop go
+
+# No snap auto-updates, we want to control when updates are installed
+snap refresh --hold=forever
 
 if [[ ("${_DISTRO}" == "ubuntu") ]]; then
     apt install ubuntu-restricted-extras mainline -y
 fi
 
 # Gnome settings
-gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
-gsettings set org.gnome.desktop.interface show-battery-percentage true
-gsettings set org.gnome.desktop.interface enable-animations false
+if [[ "${_IS_GNOME}" == true ]]; then
+    sudo apt install gnome-tweaks -y
+    gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
+    gsettings set org.gnome.desktop.interface show-battery-percentage true
+    gsettings set org.gnome.desktop.interface enable-animations false
+fi
+
+sudo apt autoremove
 
 # https://fwupd.org/lvfs/devices/
 fwupdmgr refresh
